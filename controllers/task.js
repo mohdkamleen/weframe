25de73761ed975013b6b13483b3903051a911d15
mohdkamleen <kamleenmohd@gmail.com>
2023-11-26 17:04:59 +0530

    update task controller

---
+++ controllers/task.js
@@ -0,0 +1,66 @@
+const Task = require("../models/task");
+
+// route for add new task  
+module.exports.add = async (req, res, next) => {
+    if(!(req.title,req.description,req.assignedUser,req.dueTo)){
+        return res.status(400).json({message:"All feild are required."})
+    }
+  try { 
+    const task = new Task(req.body);
+    task.save()
+    res.status(200).json(task);
+  } catch (err) {
+    next(err);
+  }
+};   
+
+// route for get all task 
+module.exports.getAll = async (req, res, next) => {
+  try {
+    const user = await Task.find();
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// route for get single data 
+module.exports.getById = async (req, res, next) => {
+    try { 
+      const task = await Task.findById(req.params.id)
+      res.status(200).json(task);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+// route for delete task
+module.exports.delete= async (req, res, next) => { 
+  try {  
+    var task = await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// route for update the task 
+module.exports.update = async (req, res, next) => {
+    try { 
+      const task = await Task.findByIdAndUpdate(req.params.id, req.body);
+      res.status(200).json(task);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // route for update the task 
+  module.exports.updateTask = async (req, res, next) => {
+    try { 
+      const task = await Task.findByIdAndUpdate(req.params.id, { $push: { status: req.body } });
+      res.status(200).json(task);
+    } catch (err) {
+      next(err);
+    }
+  };
+ 
--- routers/task.js
+++ routers/task.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const router = express.Router() 
+const router = express.Router()
+const task = require('../controllers/task')
 
+router.route('/').get(task.getAll);
+router.route('/add').post(task.add);   
+router.route('/:id')
+    .get(task.getById) 
+    .put(task.update)
+    .patch(task.updateTask)
+    .delete(task.delete)
 
 module.exports = router;
